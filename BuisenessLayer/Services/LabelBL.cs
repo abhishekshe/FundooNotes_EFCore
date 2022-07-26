509c75015c3ebf0466b9d235ac633fce2d11f64a
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-07-26 23:12:37 +0530

    Adding UC17

--- BuisenessLayer/Services/LabelBL.cs
+++ BuisenessLayer/Services/LabelBL.cs
@@ -57,11 +57,24 @@ namespace BusinessLayer.Services
             }
         }
 
-        public async Task<bool> UpdateLable(int NoteId, string Labelname)
+        public async Task<bool> UpdateLable(int UserId, int LabelId, string Labelname)
         {
             try
             {
-                return await this.labelRL.UpdateLable(NoteId, Labelname);
+                return await this.labelRL.UpdateLable(UserId, LabelId, Labelname);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<bool> DeleteLabel(int UserId, int LabelId)
+        {
+            try
+            {
+                return await this.labelRL.DeleteLabel(UserId, LabelId);
             }
             catch (Exception ex)
             {
